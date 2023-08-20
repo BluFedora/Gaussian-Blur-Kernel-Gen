c80e7877657a1dbb3df2afb053f22c53d11327a3
Shareef Raheem <BluFedora@users.noreply.github.com>
2023-08-19 20:47:51 -0700

    Update blur_gen_main.cpp
    - Removed dead link to Gaussian blur article.
    - Takes in blur radius rather than size to remove checking for if it's an odd number.
    - Pi constant calculated at runtime.
    - Memory allocations have been merged.
    - Moved all printing to the bottom of the function.
    - Fix 1D kernel from not accounting for normalization.

--- blur_gen_main.cpp
+++ blur_gen_main.cpp
@@ -1,55 +1,49 @@
 //
 // Gaussian Blur Generator
-//
-// Example Usage: ./GaussianBlurGen 7 0.84089642
-//
-// Sigma 1.8, Kernel: 9
+// 
+// Example Usage: ./GaussianBlurGen 9 1.7573
 //
 // References:
 //   https://homepages.inf.ed.ac.uk/rbf/HIPR2/gsmooth.htm
-//   http://dev.theomader.com/gaussian-kernel-calculator/
 //
 
-#include <cmath>    // exp
+#include <cmath>    // exp, acos, sqrt
 #include <cstdio>   // printf
 #include <cstdlib>  // atoi
 #include <memory>   // unique_ptr
 
 int main(int argc, char* argv[])
 {
-  static constexpr double k_TwoPI = 6.28318530718;
-
   if (argc < 3)
   {
-    std::printf("usage: %s <blur-size> <blur-variance>\n", argv[0]);
+    std::printf("usage: %s <blur-radius> <blur-variance>\n", argv[0]);
     return 1;
   }
 
-  const int blur_size = std::atoi(argv[1]);
+  const int blur_radius = std::atoi(argv[1]);
 
-  if (blur_size <= 0)
+  if (blur_radius <= 0)
   {
-    std::printf("ERROR: <blur-size> should be greater than 0 not `%i`.\n", blur_size);
+    std::printf("ERROR: <blur-radius> should be greater than 0 not `%i`.\n", blur_radius);
     return 2;
   }
 
-  if ((blur_size & 1) == 0)
-  {
-    std::printf("ERROR: <blur-size> must be an odd number not `%i`.\n", blur_size);
-    return 2;
-  }
+  const double                    variance               = std::strtod(argv[2], nullptr);
+  const int                       blur_size              = blur_radius * 2 - 1;
+  const double                    two_pi                 = 2.0 * std::acos(-1.0);
+  const double                    variance_sq            = variance * variance;
+  const double                    two_variance_sq        = 2.0 * variance_sq;
+  const double                    denom2D                = two_pi * variance_sq;
+  const double                    denom1D                = std::sqrt(two_pi) * variance;
+  const int                       num_elements           = blur_size * blur_size;
+  const int                       half_size              = blur_size / 2;
+  const std::unique_ptr<double[]> kernel_memory          = std::make_unique<double[]>(num_elements + num_elements + blur_size);
+  double* const                   blur_kernel            = kernel_memory.get();
+  double* const                   normalized_blur_kernel = blur_kernel + num_elements;
+  double* const                   oneD_kernel            = normalized_blur_kernel + num_elements;
+  double                          total_value            = 0.0f;
 
-  const double variance        = std::strtod(argv[2], nullptr);
-  const double variance_sq     = variance * variance;
-  const double two_variance_sq = 2.0 * variance_sq;
-  const double denom           = k_TwoPI * variance_sq;
-  const int    num_elements    = blur_size * blur_size;
-  const int    half_size       = blur_size / 2;
-
-  const std::unique_ptr<double[]> blur_kernel = std::make_unique<double[]>(num_elements);
-  double                          total_value = 0.0f;
-  int                             kernel_y    = 0;
-
+  int kernel_y = 0;
   for (int y = -half_size; y <= half_size; ++y)
   {
     int kernel_x = 0;
@@ -59,7 +53,7 @@ int main(int argc, char* argv[])
       const double x_sq      = double(x * x);
       const double y_sq      = double(y * y);
       const double numerator = std::exp(-(x_sq + y_sq) / two_variance_sq);
-      const double value     = numerator / denom;
+      const double value     = numerator / denom2D;
 
       blur_kernel[kernel_x + kernel_y * blur_size] = value;
       total_value += value;
@@ -70,10 +64,33 @@ int main(int argc, char* argv[])
     ++kernel_y;
   }
 
-  const auto printMatrix = [blur_size](const std::unique_ptr<double[]>& matrix) {
+  double normalized_total = 0.0;
+  for (int i = 0; i < num_elements; ++i)
+  {
+    normalized_blur_kernel[i] = blur_kernel[i] / total_value;
+    normalized_total += normalized_blur_kernel[i];
+  }
+
+  double oneDTotal = 0.0;
+  int xx = -half_size;
+  for (int i = 0; i < blur_size; ++i)
+  {
+    const double x_sq           = double(xx * xx);
+    const double numerator      = std::exp(-x_sq / two_variance_sq);
+    const double one_d_gaussian = numerator / denom1D;
+
+    oneD_kernel[i] = one_d_gaussian;
+    oneDTotal += one_d_gaussian;
+
+    ++xx;
+  }
+
+  const auto PrintMatrix2D = [&](const char* title, const double total, const double* const matrix) {
+
+    std::printf("%s(%ix%i, sigma = %f)(Total:%f):\n", title, blur_size, blur_size, variance, total);
     for (int y = 0; y < blur_size; ++y)
     {
-      std::printf("| ");
+      std::printf("  | ");
 
       for (int x = 0; x < blur_size; ++x)
       {
@@ -82,40 +99,32 @@ int main(int argc, char* argv[])
 
       std::printf("|\n");
     }
-  };
+    };
 
-  const std::unique_ptr<double[]> normalized_blur_kernel = std::make_unique<double[]>(num_elements);
+  PrintMatrix2D("Gaussian2D", total_value, blur_kernel);
+  std::printf("\n");
+  PrintMatrix2D("Gaussian2D_normalized", normalized_total, normalized_blur_kernel);
+  std::printf("\n");
 
-  for (int i = 0; i < num_elements; ++i)
+  std::printf("Gaussian1D(Total:%f):\n", oneDTotal);
   {
-    normalized_blur_kernel[i] = blur_kernel[i] / total_value;
+    int xx = -half_size;
+
+    for (int i = 0; i < blur_size; ++i)
+    {
+      const int index = i + i * blur_size;
+
+      std::printf("  [%3i] = {orig(%.15f), orig_sqrt(%.15f), norm(%.15f), norm_sqrt(sqrt = %.15f), 1d(%.15f), 1d_norm(%.15f) }\n", 
+        xx, 
+        blur_kernel[index], 
+        std::sqrt(blur_kernel[index]),
+        normalized_blur_kernel[index], 
+        std::sqrt(normalized_blur_kernel[index]), 
+        oneD_kernel[i],
+        oneD_kernel[i] / oneDTotal);
+      ++xx;
+    }
+
   }
-
-  std::printf("Unnormalized Matrix(Total:%f):\n", total_value);
-  printMatrix(blur_kernel);
-
-  std::printf("\nNormalized Matrix:\n");
-  printMatrix(normalized_blur_kernel);
-
-  std::printf("\nWeights:\n");
-
-  const double denom1D = std::sqrt(k_TwoPI * variance);
-  int          xx      = -half_size;
-
-  for (int i = 0; i < blur_size; ++i)
-  {
-    const double x_sq           = double(xx * xx);
-    const double numerator      = std::exp(-x_sq / two_variance_sq);
-    const double one_d_gaussian = numerator / denom1D;
-
-    std::printf("[%3i] = %.15f => %.15f (%.15f)\n",
-                xx,
-                normalized_blur_kernel[i + i * blur_size],
-                std::sqrt(normalized_blur_kernel[i + i * blur_size]),
-                one_d_gaussian);
-
-    ++xx;
-  }
-
   return 0;
 }
